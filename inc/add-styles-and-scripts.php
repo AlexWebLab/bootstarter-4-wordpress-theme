8481dc1f4a6837ae39b7d342779c557f9123a98c
Alessandro <info@alexweblab.com>
2018-05-15 12:05:49 +0800

    added FontAwesome 5.0.13

--- inc/add-styles-and-scripts.php
+++ inc/add-styles-and-scripts.php
@@ -6,6 +6,9 @@ if( !is_admin() ){ // add them only on the theme
     wp_enqueue_script('jquery', get_stylesheet_directory_uri().'/js/jquery-3.3.1.slim.min.js', array(), '3.1.1', true);
     wp_enqueue_script('bootstrap', get_stylesheet_directory_uri().'/js/bootstrap.bundle.min.js', array('jquery'), '4.1.1', true);
 
+    // FontAwesome
+    wp_enqueue_style('fontawesome', get_stylesheet_directory_uri().'/css/fontawesome-all.min.css', array(), '5.0.13');
+
     // Custom styles and scripts
     wp_enqueue_style('custom', get_stylesheet_directory_uri().'/css/custom.css', array('bootstrap'), time());
     wp_enqueue_script('custom', get_stylesheet_directory_uri().'/js/custom.js', array('bootstrap'), time(), true);
