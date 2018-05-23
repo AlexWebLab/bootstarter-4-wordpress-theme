f99e68bd0dd3c904cbae79e30434b019c8c3d2ce
Alessandro <info@alexweblab.com>
2018-05-23 17:08:09 +0800

    fixed adding custom styles and scripts

--- inc/add-styles-and-scripts.php
+++ inc/add-styles-and-scripts.php
@@ -1,9 +1,9 @@
 <?php
-if( !is_admin() ){ // add them only on the theme
+function add_styles_and_scripts() {
     // Bootstrap 4
     wp_enqueue_style('bootstrap', get_stylesheet_directory_uri().'/css/bootstrap.min.css', array(), '4.1.1');
     wp_deregister_script('jquery');
-    wp_enqueue_script('jquery', get_stylesheet_directory_uri().'/js/jquery-3.3.1.slim.min.js', array(), '3.1.1', true);
+    wp_enqueue_script('jquery', get_stylesheet_directory_uri().'/js/jquery-3.3.1.min.js', array(), '3.1.1', true);
     wp_enqueue_script('bootstrap', get_stylesheet_directory_uri().'/js/bootstrap.bundle.min.js', array('jquery'), '4.1.1', true);
 
     // FontAwesome
@@ -13,3 +13,4 @@ if( !is_admin() ){ // add them only on the theme
     wp_enqueue_style('custom', get_stylesheet_directory_uri().'/css/custom.css', array('bootstrap'), time());
     wp_enqueue_script('custom', get_stylesheet_directory_uri().'/js/custom.js', array('bootstrap'), time(), true);
 }
+add_action( 'wp_enqueue_scripts', 'add_styles_and_scripts' );
